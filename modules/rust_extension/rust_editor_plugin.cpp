6d1439dfdb0fde776e8fd18e08712b944e395c2f
webstorm_hpcg <dxy20070515@163.com>
2025-08-10 18:07:51 +0800

    feat(rust_editor_plugin): 新增创建 Rust 对象的功能
    - 在 RustEditorPlugin 中添加了创建新 Rust 对象的菜单项和相关功能
    - 实现了创建新 Rust 对象的窗口界面，包括类名输入、路径选择等功能- 添加了创建新 Rust 对象的逻辑和信号连接

--- modules/rust_extension/rust_editor_plugin.cpp
+++ modules/rust_extension/rust_editor_plugin.cpp
@@ -7,13 +7,17 @@
 #include "editor/gui/editor_title_bar.h"
 #include "modules/mono/utils/path_utils.h"
 #include "scene/gui/button.h"
+#include "scene/gui/file_dialog.h"
+#include "scene/gui/label.h"
+#include "scene/gui/line_edit.h"
 #include "scene/gui/popup_menu.h"
 
 RustEditorPlugin::RustEditorPlugin() {
 	submenu = memnew(PopupMenu);
-	submenu->add_item(TTR("Initial Rust Project"), 0);
-	submenu->add_item(TTR("Cargo Build"), 1);
-	submenu->add_item(TTR("Cargo Add Godot"), 2);
+	submenu->add_item(TTR("Initial Rust Project"), INIT);
+	submenu->add_item(TTR("Cargo Build"), BUILD);
+	submenu->add_item(TTR("Cargo Add Godot"), ADD_DEPS);
+	submenu->add_item(TTR("Create New Rust Object"), NEW_OBJECT);
 	submenu->connect("id_pressed", callable_mp(this, &RustEditorPlugin::_submenu_callback));
 	submenu->add_separator();
 	add_tool_submenu_item(TTR("Rust Extension"), submenu);
@@ -38,7 +42,7 @@ void RustEditorPlugin::_submenu_callback(int p_id) {
 	Error add_deps_err;
 	Error init_err;
 	switch (p_id) {
-		case 0:
+		case INIT:
 			init_err = CargoTool::Cargo()->set_work_dir(work_dir)->init()->done();
 			if (init_err != OK) {
 				ERR_PRINT("Cargo init Failed");
@@ -52,24 +56,113 @@ void RustEditorPlugin::_submenu_callback(int p_id) {
 				ERR_PRINT("Cargo build Failed");
 			}
 			break;
-		case 1:
+		case BUILD:
 			print_line("cargo build");
 			build_err = CargoTool::Cargo()->set_work_dir(work_dir)->build()->done();
 			if (build_err != OK) {
 				ERR_PRINT("Cargo build Failed");
 			}
 			break;
-		case 2:
+		case ADD_DEPS:
 			add_deps_err = CargoTool::Cargo()->set_work_dir(work_dir)->add("godot")->done();
 			if (add_deps_err != OK) {
 				ERR_PRINT("Cargo add godot Failed");
 			}
+			break;
+		case NEW_OBJECT:
+			_create_new_rust_object();
 			break;
 		default:
 			ERR_PRINT("Cargo Failed");
 	}
 }
 
+void RustEditorPlugin::_create_new_rust_object() {
+	print_line("Create new rust object");
+	//TODO:修复UI形态的问题
+	create_windows = memnew(Window);
+	create_windows->set_title(TTR("New Rust Object"));
+	create_windows->set_position((DisplayServer::get_singleton()->screen_get_size() / 2));
+	create_windows->set_size(Size2i(500, 300));
+
+	VBoxContainer* vbox = memnew(VBoxContainer);
+	vbox->set_h_size_flags(Control::SIZE_FILL);
+	vbox->set_alignment(BoxContainer::ALIGNMENT_CENTER);
+
+	// 类名输入部分
+	Label* name_label = memnew(Label);
+	name_label->set_text(TTR("Class Name:"));
+
+	HBoxContainer* name_hbox = memnew(HBoxContainer);
+	name_hbox->set_h_size_flags(Control::SIZE_FILL);
+	vbox->add_child(name_hbox);
+
+	name_hbox->add_child(name_label);
+
+	LineEdit* name_input = memnew(LineEdit);
+	name_input->set_placeholder(TTR("Enter Class Name By snake_case"));
+	name_input->set_h_size_flags(Control::SIZE_FILL);
+	name_input->set_horizontal_alignment(HORIZONTAL_ALIGNMENT_CENTER);
+	name_hbox->add_child(name_input);
+
+	// 路径选择部分
+	Label* path_label = memnew(Label);
+	path_label->set_text(TTR("Path:"));
+
+	HBoxContainer* path_hbox = memnew(HBoxContainer);
+	path_hbox->set_h_size_flags(Control::SIZE_EXPAND_FILL);
+	vbox->add_child(path_hbox);
+
+	path_hbox->add_child(path_label);
+
+	LineEdit* path_input = memnew(LineEdit);
+	path_input->set_placeholder(TTR("Select Path:"));
+	path_input->set_editable(false);
+	path_input->set_h_size_flags(Control::SIZE_EXPAND_FILL);
+	path_input->set_horizontal_alignment(HORIZONTAL_ALIGNMENT_CENTER);
+	path_hbox->add_child(path_input);
+
+	Button* browse_button = memnew(Button);
+	browse_button->set_text(TTR("Browse"));
+	path_hbox->add_child(browse_button);
+
+	FileDialog* file_dialog = memnew(FileDialog);
+	file_dialog->set_access(FileDialog::ACCESS_RESOURCES);
+	file_dialog->set_file_mode(FileDialog::FILE_MODE_SAVE_FILE);
+	file_dialog->set_show_hidden_files(false);
+	create_windows->add_child(file_dialog);
+
+	// 按钮区域
+	HBoxContainer* button_hbox = memnew(HBoxContainer);
+	button_hbox->set_alignment(BoxContainer::ALIGNMENT_CENTER);
+	vbox->add_child(button_hbox);
+
+	Button* ok_button = memnew(Button);
+	ok_button->set_text(TTR("OK"));
+	ok_button->set_custom_minimum_size(Size2(100, 0));
+	button_hbox->add_child(ok_button);
+
+	Button* cancel_button = memnew(Button);
+	cancel_button->set_text(TTR("Cancel"));
+	cancel_button->set_custom_minimum_size(Size2(100, 0));
+	button_hbox->add_child(cancel_button);
+
+	create_windows->add_child(vbox);
+
+	// 连接信号
+	browse_button->connect("pressed", callable_mp(file_dialog, &FileDialog::FileDialog::popup_file_dialog));
+	file_dialog->connect("dir_selected", callable_mp(path_input, &LineEdit::set_text));
+	cancel_button->connect("pressed", callable_mp(this, &RustEditorPlugin::_free_windows_object));
+
+	create_windows->connect("close_requested", callable_mp(this, &RustEditorPlugin::_free_windows_object));
+	EditorNode::get_singleton()->add_child(create_windows);
+}
+
+void RustEditorPlugin::_free_windows_object() {
+	create_windows->queue_free();
+}
+
+//TODO:添加快捷键绑定
 void RustEditorPlugin::_build_button_callback() {
 	print_line("Run Cargo Building...");
 	const String work_dir = ProjectSettings::get_singleton()->get_resource_path();
--- modules/rust_extension/rust_editor_plugin.h
+++ modules/rust_extension/rust_editor_plugin.h
@@ -11,10 +11,20 @@ public:
 
 	PopupMenu* submenu;
 	Button* build_button;
+	Window* create_windows;
 	void _build_button_callback();
+
+	enum RustMenuID {
+		INIT,
+		BUILD,
+		ADD_DEPS,
+		NEW_OBJECT
+	};
 
 private:
 	void _submenu_callback(int p_id);
+	void _create_new_rust_object();
+	void _free_windows_object();
 
 protected:
 	static void _bind_methods();
@@ -22,7 +32,7 @@ protected:
 
 class BuildButton: public Button{
 	GDCLASS(BuildButton, Button)
-	
+
 protected:
 	void _notification(int p_what);
 
