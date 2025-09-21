593060aff2cb9244c020015b420d742a12c78b00
webstorm_hpcg <dxy20070515@163.com>
2025-09-21 20:58:05 +0800

    修复编译bug

--- modules/gorge_blueprint/register_types.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "modules/register_module_types.h"
-
-void initialize_gorge_blueprint_module(ModuleInitializationLevel p_level);
-
-void uninitialize_gorge_blueprint_module(ModuleInitializationLevel p_level);
